38a81dbd5f822e7b3caadc56131a74827f9047f8
wafaadam548 <wafaadam548@gmail.com>
2026-04-30 13:04:29 -0400

    mae the player able to exit the game

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ pg.init()
 width=100
 height=500
 pg.display.set_mode((width,height))
+while True:
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            pg.quit()
+    pg.display.update()
