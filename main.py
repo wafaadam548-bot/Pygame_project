27932541dc3288e722ac27542ef9802525cc9889
wafaadam548 <wafaadam548@gmail.com>
2026-05-03 01:44:02 -0400

    trying test surface

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from sys import exit
 pg.init()
 width=1000
 height=500
-pg.display.set_mode((width,height))
+screen=pg.display.set_mode((width,height))
+pg.display.set_caption("Escape Run  ")#change the name in the caption
+clock=pg.time.Clock()#it's going to help with the frame rate 
+test_surface=pg.Surface((100,200))
+test_surface.fill("White")
 while True:
     for event in pg.event.get():
         if event.type==pg.QUIT:
-            pg.quit()
-            exit()
+            pg.quit()#tell python to shut down all the pygame moudel 
+            exit()#telling python to stop running the entire file
+    screen.blit(test_surface,(200,100))
     pg.display.update()
+    clock.tick(60)#Consistent Game Speed
