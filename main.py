30f52b0f5ab3df06aa20f5922fb4e0e0e91c203e
wafaadam548 <wafaadam548@gmail.com>
2026-05-03 02:31:23 -0400

    resize the image

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ height=500
 screen=pg.display.set_mode((width,height))
 pg.display.set_caption("Escape Run  ")#change the name in the caption
 clock=pg.time.Clock()#it's going to help with the frame rate 
-test_surface=pg.image.load("Image/ground.png")
-test_surface.fill("White")
+sky_imaage=pg.image.load("Image/sky.jpg")
+sky_imaage=pg.transform.scale(sky_imaage,(1000,400))#resize the image
 while True:
     for event in pg.event.get():
         if event.type==pg.QUIT:
             pg.quit()#tell python to shut down all the pygame moudel 
             exit()#telling python to stop running the entire file
-    screen.blit(test_surface,(0,0))
+    screen.blit(sky_imaage,(0,0))
     pg.display.update()
     clock.tick(60)#Consistent Game Speed
