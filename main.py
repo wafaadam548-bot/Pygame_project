59298101b60f01ffc8b50d5de68a05c0c208f6b9
wafaadam548 <wafaadam548@gmail.com>
2026-05-03 03:39:21 -0400

    use convert alpha so the game is going to run faster and pygame will work with it easeirer

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ screen=pg.display.set_mode((width,height))
 pg.display.set_caption("Escape Run  ")#change the name in the caption
 clock=pg.time.Clock()#it's going to help with the frame rate 
 score=pg.font.Font("Font/Archivo_Black/ArchivoBlack-Regular.ttf",50)
-sky_imaage=pg.image.load("Image/sky.jpg")
+sky_imaage=pg.image.load("Image/sky.jpg").convert_alpha
 sky_imaage=pg.transform.scale(sky_imaage,(1000,400))#resize the image
-ground_image=pg.image.load("Image/ground.png")
+ground_image=pg.image.load("Image/ground.png").convert_alpha
 ground_image=pg.transform.scale(ground_image,(1000,270))
-enemy_image=pg.image.load("Image/enemy.png")
+enemy_image=pg.image.load("Image/enemy.png").convert_alpha
+enemy_image=pg.transform.scale(enemy_image,(130,105))
 enemy_x_position=800
 score_image=score.render("Hi",False,"Black")
 while True:
@@ -20,10 +21,10 @@ while True:
             pg.quit()#tell python to shut down all the pygame moudel 
             exit()#telling python to stop running the entire file
     screen.blit(sky_imaage,(0,0))
-    screen.blit(ground_image,(0,360))
+    screen.blit(ground_image,(0,340))
     screen.blit(score_image,(width/2,height/2))
     enemy_x_position-=6
-    screen.blit(enemy_image,(enemy_x_position,300))
+    screen.blit(enemy_image,(enemy_x_position,310))
     if enemy_x_position<=-100:
         enemy_x_position=1000
     pg.display.update()
