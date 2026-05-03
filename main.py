d69644a539cc839a5ba21f0930d6332b2f770ab2
wafaadam548 <wafaadam548@gmail.com>
2026-05-03 03:55:38 -0400

    upload the player srfase in the screen

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ screen=pg.display.set_mode((width,height))
 pg.display.set_caption("Escape Run  ")#change the name in the caption
 clock=pg.time.Clock()#it's going to help with the frame rate 
 score=pg.font.Font("Font/Archivo_Black/ArchivoBlack-Regular.ttf",50)
-sky_imaage=pg.image.load("Image/sky.jpg").convert_alpha
+sky_imaage=pg.image.load("Image/sky.jpg").convert_alpha()
 sky_imaage=pg.transform.scale(sky_imaage,(1000,400))#resize the image
-ground_image=pg.image.load("Image/ground.png").convert_alpha
+ground_image=pg.image.load("Image/ground.png").convert_alpha()
 ground_image=pg.transform.scale(ground_image,(1000,270))
-enemy_image=pg.image.load("Image/enemy.png").convert_alpha #pygame is going to work with conver.alpha faster 
+enemy_image=pg.image.load("Image/enemy.png").convert_alpha() #pygame is going to work with conver.alpha faster 
 enemy_image=pg.transform.scale(enemy_image,(130,105))
+player1=pg.image.load("Image/Player/Player1.png").convert_alpha()
+player1=pg.transform.scale(player1,(100,75))
+
 enemy_x_position=800
 score_image=score.render("Hi",False,"Black")
 while True:
@@ -25,6 +28,7 @@ while True:
     screen.blit(score_image,(width/2,height/2))
     enemy_x_position-=6
     screen.blit(enemy_image,(enemy_x_position,310))
+    screen.blit(player1,(20,310))
     if enemy_x_position<=-100:
         enemy_x_position=1000
     pg.display.update()
