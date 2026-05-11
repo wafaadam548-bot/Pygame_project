ad318a280249a62fb40fb6e3b3c32a1dcfd75329
wafaadam548 <wafaadam548@gmail.com>
2026-05-10 22:00:21 -0400

    set teh sore and the score massege and if the player touch the coins it will disappere and the score will increase

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ width=1000
 height=500
 game_active=True
 def display_score():
-    time=int(pg.time.get_ticks()/1000)-start_time
-    score_image=font.render(f"{time}",False,"Black")
+    score_image=font.render(f"{score}",False,"Black")
     score_rect=score_image.get_rect(center=(400,50))
     screen.blit(score_image,score_rect)
 
@@ -22,18 +21,24 @@ sky_imaage=pg.transform.scale(sky_imaage,(1000,400))#resize the image
 ground_image=pg.image.load("Image/ground.png").convert_alpha()
 ground_image=pg.transform.scale(ground_image,(1000,270))
 enemy_image=pg.image.load("Image/enemy.png").convert_alpha() #pygame is going to work with conver.alpha faster 
-enemy_image=pg.transform.scale(enemy_image,(130,105))
+enemy_image=pg.transform.scale(enemy_image,(120,95))
 enemy_rect=enemy_image.get_rect(midbottom=(800,400))
 player1=pg.image.load("Image/Player/Player1.png").convert_alpha()
 player1=pg.transform.scale(player1,(100,75))
 move=50
+coins=pg.image.load("Image/coins.png").convert_alpha()
+coins=pg.transform.scale(coins,(70,70))
 player2=pg.image.load("Image/Player/Player2.png").convert_alpha()
 start_time=0
 player1_rect=player1.get_rect(midbottom=(move,400))
-score=display_score()
+score=0
 enemy_x_position=800
-game_over=score.render("Game over. Press space to play again =) ", False,"Black")
+game_over=font.render("Game over. Press space to play again =) ", False,"Black")
 player_gravity=0
+coins_rect=[]
+for i in range(5):
+    coin_rect=coins.get_rect(midbottom=(100+ i * 200,350))#the i represent the space betwen each coin
+    coins_rect.append(coin_rect)
 while True:
     for event in pg.event.get():
         if event.type==pg.QUIT:
@@ -43,7 +48,7 @@ while True:
             if event.type==pg.KEYDOWN:
                 if player1_rect.bottom>=380:# the player can't jump until the player tuch the ground 
                     if event.key==pg.K_UP:
-                        player_gravity=-20
+                        player_gravity=-24
                 if event.key==pg.K_LEFT:
                     player1_rect.right-=3  
                 if event.key==pg.K_RIGHT:
@@ -55,10 +60,27 @@ while True:
                     game_active=True
                     enemy_rect.left=1000 
                     start_time=int(pg.time.get_ticks()/1000)
+                    score=0
+                    coins_rect=[]
+                    for i in range(5):
+                        coin_rect=coins.get_rect(midbottom=(100+ i * 200,350))#the i represent the space betwen each coin
+                        coins_rect.append(coin_rect)
+                    player_gravity = 0
+                    player1_rect.midbottom = (move, 400)
     if game_active:            
         screen.blit(sky_imaage,(0,0))
         screen.blit(ground_image,(0,340))
         #enemy_x_position-=6
+        for coin_rect in coins_rect[:]:
+            screen.blit(coins, coin_rect)
+
+            coin_rect.x -= 2
+
+            if coin_rect.right <= 0:
+                coin_rect.left = 1000
+            if player1_rect.colliderect(coin_rect):
+                coins_rect.remove(coin_rect)
+                score += 1
         screen.blit(enemy_image,(enemy_rect))
         player_gravity+=1
         player1_rect.y+=player_gravity
@@ -66,15 +88,19 @@ while True:
             player1_rect.bottom=380
         screen.blit(player1,player1_rect)
         display_score()
+
         enemy_rect.left-=4
         if enemy_rect.right<=-0:
             enemy_rect.left=1000
         if enemy_rect.colliderect(player1_rect):#if the player touch the enemy
             game_active=False
+            
     else:
         screen.fill((120,225,225))
         screen.blit(game_over,(200,height/2))
         screen.blit(player2,(100,200))
-
+        score_massege=font.render(f"Your Score is {score}",False,(0,0,0))
+        score_massege_rect=score_massege.get_rect(center=(500,330))
+        screen.blit(score_massege,score_massege_rect)
     pg.display.update()
     clock.tick(60)#Consistent Game Speed
